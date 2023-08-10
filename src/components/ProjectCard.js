e4c60b96b3485206b3e9cfb4e834bf0c51ea831c
ruishanteo <ruishan.teo03@gmail.com>
2023-08-11 07:45:30 +0800

    added project page

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,6 @@ import {
   Box,
   Button,
   Card,
-  CardActions,
   CardContent,
   CardMedia,
   Dialog,
@@ -12,13 +11,33 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Grid,
+  Link,
+  Tooltip,
   Typography,
   useMediaQuery,
 } from "@mui/material";
 
 import { useTheme } from "@mui/material/styles";
 
-export function ProjectCard() {
+const renderTech = (techName, techStack) => {
+  let srcUrl = "";
+  techStack.forEach((techData) => {
+    if (techData.id === techName.toLowerCase()) {
+      srcUrl = techData.iconImg;
+    }
+  });
+
+  return (
+    <Grid item key={techName}>
+      <Tooltip title={techName}>
+        <img src={srcUrl} alt={techName} width={30} />
+      </Tooltip>
+    </Grid>
+  );
+};
+
+export function ProjectCard({ project, techStack }) {
   const [open, setOpen] = React.useState(false);
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
@@ -32,52 +51,144 @@ export function ProjectCard() {
   };
 
   return (
-    <Card sx={{ maxWidth: 345, mt: 3 }}>
-      <CardMedia
-        component="img"
-        alt="project1"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-      />
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h5"
-          fontFamily="Roboto"
-          component="div"
+    <Box>
+      <Button
+        onClick={handleClickOpen}
+        sx={{
+          "&:hover": {
+            backgroundColor: "primary.dark",
+          },
+          borderRadius: 3,
+          padding: 2,
+        }}
+      >
+        <Card
+          sx={{
+            borderRadius: 3,
+            width: { xs: 300, md: 400 },
+            height: 500,
+          }}
         >
-          Project1
-        </Typography>
-        <Typography fontFamily="Roboto" variant="body1" color="text.secondary">
-          text description
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">GitHub</Button>
-        <Button size="small" onClick={handleClickOpen}>
-          Learn More
-        </Button>
-        <Dialog
-          fullScreen={fullScreen}
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="responsive-dialog-title"
-        >
-          <DialogTitle id="responsive-dialog-title" variant="h5">
-            {"Project1"}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText variant="body2">
-              more about the project
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button autoFocus onClick={handleClose}>
-              Close
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </CardActions>
-    </Card>
+          <CardMedia
+            component="img"
+            alt="project1"
+            height={250}
+            image={project.thumbnailImg}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {project.title}
+            </Typography>
+
+            <Box
+              sx={{
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                display: "-webkit-box",
+                WebkitLineClamp: "2",
+                WebkitBoxOrient: "vertical",
+                mb: 3,
+              }}
+            >
+              <Typography
+                variant="body1"
+                color="text.secondary"
+                sx={{ wordBreak: "break-word", textTransform: "none" }}
+              >
+                {project.description}
+              </Typography>
+            </Box>
+            <Grid container>
+              {project.techStack.map((tech) => renderTech(tech, techStack))}
+            </Grid>
+          </CardContent>
+        </Card>
+      </Button>
+
+      <Dialog
+        fullScreen={fullScreen}
+        open={open}
+        onClose={handleClose}
+        fullWidth
+      >
+        <DialogTitle>
+          <Box
+            sx={{
+              backgroundColor: "secondary.dark",
+              height: "5vh",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+              {project.title}
+            </Typography>
+          </Box>
+        </DialogTitle>
+
+        <Box align="center">
+          <img width="90%" src={project.fullSizedImg} alt="fullSized img" />
+        </Box>
+
+        <DialogContent>
+          <DialogContentText variant="h6" sx={{ mt: 3, fontWeight: "bold" }}>
+            Description
+          </DialogContentText>
+
+          <DialogContentText variant="body1">
+            {project.description}
+          </DialogContentText>
+
+          <DialogContentText variant="h6" sx={{ mt: 3, fontWeight: "bold" }}>
+            Start Date:
+          </DialogContentText>
+
+          <DialogContentText variant="body1">
+            {project.startDate}
+          </DialogContentText>
+
+          <DialogContentText variant="h6" sx={{ mt: 3, fontWeight: "bold" }}>
+            End Date:
+          </DialogContentText>
+
+          <DialogContentText variant="body1">
+            {project.endDate}
+          </DialogContentText>
+
+          <DialogContentText variant="h6" sx={{ mt: 3, fontWeight: "bold" }}>
+            Tech Stack
+          </DialogContentText>
+
+          <Grid container flexDirection="row">
+            {project.techStack.map((tech) => renderTech(tech, techStack))}
+          </Grid>
+          <DialogContentText variant="h6" sx={{ mt: 3, fontWeight: "bold" }}>
+            Links
+          </DialogContentText>
+
+          {project.links.map((link) => {
+            return (
+              <Link href={`${link.url}`} key={link.displayText}>
+                <Typography variant="body1" sx={{ color: "primary.dark" }}>
+                  {link.displayText}
+                </Typography>
+              </Link>
+            );
+          })}
+        </DialogContent>
+
+        <DialogActions>
+          <Button
+            autoFocus
+            onClick={handleClose}
+            variant="outlined"
+            color="secondary"
+          >
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
   );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,15 +1,59 @@
-import * as React from "react";
-import { ProjectCard } from "../components/ProjectCard";
+import { useCallback, useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { Box, Typography } from "@mui/material";
+import { ProjectCard } from "../components/ProjectCard";
+import { fetchProjects } from "../backend/projectsStore";
+import { fetchTechStack } from "../backend/techStackStore";
+import { LoadingIcon } from "../components/LoadingIcon";
+
+import { Box, Grid, Typography } from "@mui/material";
+
+import { StarBorderOutlined } from "@mui/icons-material/";
 
 export function Projects() {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+
+  const projects = useSelector((state) => state.projects.projects);
+  const techStack = useSelector((state) => state.techStack.techStack);
+
+  const onUpdate = useCallback(async () => {
+    setLoading(true);
+    await dispatch(fetchProjects);
+    await dispatch(fetchTechStack);
+    setLoading(false);
+  }, [dispatch]);
+
+  useEffect(() => {
+    onUpdate();
+  }, [onUpdate]);
+
   return (
-    <Box mt={5} align="center">
-      <Typography sx={{ fontWeight: 600, fontFamily: "Roboto" }} variant="h4">
+    <Box mt={3} mb={5} align="center">
+      <Typography sx={{ mb: 3 }} variant="h4">
         projects i have worked on
       </Typography>
-      <ProjectCard />
+
+      {loading ? (
+        <LoadingIcon />
+      ) : projects.length === 0 ? (
+        <Box>
+          <Typography>No projects found!</Typography>
+        </Box>
+      ) : (
+        <Box>
+          <Grid container justifyContent="center" spacing={3} sx={{ mb: 10 }}>
+            {projects.map((project) => {
+              return (
+                <Grid item key={project.id}>
+                  <ProjectCard project={project} techStack={techStack} />
+                </Grid>
+              );
+            })}
+          </Grid>
+          <StarBorderOutlined />
+        </Box>
+      )}
     </Box>
   );
 }
