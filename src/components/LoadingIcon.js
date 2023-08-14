c74d3c86f6e4e20a779c5c5e32e6a755ac4f06f5
ruishanteo <ruishan.teo03@gmail.com>
2023-08-14 09:42:21 +0800

    added loading icon

---
+++ src/components/LoadingIcon.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export const LoadingIcon = () => (
+  <svg
+    version="1.1"
+    id="Layer_1"
+    viewBox="0 0 100 100"
+    width="100px"
+    height="100px"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle className="ball" cx="45" cy="35" r="12" fill="#cbe0fb">
+      <animate
+        attributeName="cy"
+        values="65;30;65"
+        dur="1.5s"
+        begin="0.75s"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <circle
+      className="ball"
+      cx="50"
+      cy="65"
+      r="12"
+      fill="none"
+      stroke="#053e85"
+      strokeWidth="2"
+    >
+      <animate
+        attributeName="cy"
+        values="65;30;65"
+        dur="1s"
+        begin="0.5s"
+        repeatCount="indefinite"
+      />
+    </circle>
+  </svg>
+);
