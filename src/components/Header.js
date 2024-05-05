81b7ecc823073c3536ecb35b3863a248ef319838
Rui Shan <ruishan.teo03@gmail.com>
2024-05-05 16:51:20 +0800

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,133 +1,93 @@
 import React from "react";
+import {
+  AppBar,
+  Box,
+  Container,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
-import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
-
-export function Header() {
+export function Header({ value, setValue, scrollToSection, numOfSections }) {
   return (
     <AppBar
-      position="static"
       sx={{
-        backgroundColor: "primary.main",
+        background: "transparent",
+        boxShadow: "none",
         color: "inherit",
+        mt: 5,
       }}
     >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Typography
-            variant="h3"
-            sx={{
-              justifyContent: "center",
-              display: { xs: "none", md: "flex" },
-              flexGrow: 1,
-              textDecoration: "none",
-              marginLeft: 22,
-              fontFamily: "Roboto",
-              color: "inherit",
-            }}
-          >
-            <Link
-              to="/home"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              rui shan
-            </Link>
-          </Typography>
-
-          <Typography
-            variant="h4"
-            sx={{
-              justifyContent: "center",
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              textDecoration: "none",
-              fontFamily: "Roboto",
-              color: "inherit",
-            }}
-          >
-            <Link
-              to="/home"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              rui shan
-            </Link>
-          </Typography>
           <Box
             sx={{
-              flexGrow: -10,
               display: "flex",
-              flexDirection: "row",
+              flexGrow: 1,
             }}
           >
-            <Link
-              to="/projects"
-              style={{ textDecoration: "none", color: "inherit" }}
+            <Typography
+              onClick={() => scrollToSection(0)}
+              variant="h3"
+              sx={{
+                cursor: "pointer",
+                textDecoration: "none",
+                fontFamily: "Roboto",
+                color: "inherit",
+              }}
             >
-              <Typography
-                sx={{
-                  display: { xs: "none", md: "flex" },
-                  fontFamily: "Roboto",
-                }}
-                variant="h6"
-              >
-                projects
-              </Typography>
-            </Link>
-
-            <Link
-              to="/about"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              <Typography
-                sx={{
-                  display: { xs: "none", md: "flex" },
-                  fontFamily: "Roboto",
-                  ml: 5,
-                }}
-                variant="h6"
-              >
-                about
-              </Typography>
-            </Link>
+              rui shan
+            </Typography>
           </Box>
 
           <Box
             sx={{
-              flexGrow: -5,
               display: "flex",
               flexDirection: "row",
             }}
           >
-            <Link
-              to="/projects"
-              style={{ textDecoration: "none", color: "inherit" }}
+            <Tabs
+              value={value}
+              sx={{
+                ".MuiTabs-indicator": {
+                  backgroundColor: "primary.dark",
+                },
+              }}
             >
-              <Typography
-                sx={{
-                  display: { xs: "flex", md: "none" },
-                  fontFamily: "Roboto",
-                }}
-                variant="h6"
-              >
-                projects
-              </Typography>
-            </Link>
-
-            <Link
-              to="/about"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              <Typography
-                sx={{
-                  display: { xs: "flex", md: "none" },
-                  fontFamily: "Roboto",
-                  ml: 5,
-                }}
-                variant="h6"
-              >
-                about
-              </Typography>
-            </Link>
+              <Tab
+                value={1}
+                onClick={() => scrollToSection(1)}
+                label={
+                  <Typography
+                    sx={{
+                      color: "primary.dark",
+                      fontFamily: "Roboto",
+                      textTransform: "none",
+                    }}
+                    variant="h6"
+                  >
+                    projects
+                  </Typography>
+                }
+              />
+              <Tab
+                value={2}
+                onClick={() => scrollToSection(numOfSections - 3.95)}
+                label={
+                  <Typography
+                    sx={{
+                      color: "primary.dark",
+                      fontFamily: "Roboto",
+                      textTransform: "none",
+                    }}
+                    variant="h6"
+                  >
+                    about
+                  </Typography>
+                }
+              />
+            </Tabs>
           </Box>
         </Toolbar>
       </Container>
